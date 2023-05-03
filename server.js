7b2b0e728b730a0de9b916d6f58200b2ecebb7d6
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-05-03 17:26:35 +0200

    added docker example

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const port = 80
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
